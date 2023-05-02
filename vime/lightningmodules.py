7a0d5b57dbf5dec950c4fcaeea1ab92aba50f7bf
gkswotn12345 <84672164+Jaesu26@users.noreply.github.com>
2023-05-02 19:00:35 +0900

    Remove redundant code

--- vime/lightningmodules.py
+++ vime/lightningmodules.py
@@ -134,8 +134,7 @@ class VIMESemi(pl.LightningModule):
                 X_tilde, _ = pretext_generator(X_unlabeled, mask, self.random_state)
                 X_augmented.append(X_tilde)
             # https://pytorch.org/docs/stable/generated/torch.nn.BatchNorm1d.html
-            X_augmented = torch.stack(X_augmented, dim=2)  # Shape: (B, C, K)
-            batch["unlabeled"] = X_augmented
+            batch["unlabeled"] = torch.stack(X_augmented, dim=2)  # Shape: (B, C, K)
         return batch
 
     def on_train_epoch_start(self) -> None:
