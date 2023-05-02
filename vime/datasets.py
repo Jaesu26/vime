092286fa2b668022ab5dd022df470061672c2178
gkswotn12345 <84672164+Jaesu26@users.noreply.github.com>
2023-05-02 20:52:49 +0900

    Fix shape error

--- vime/datasets.py
+++ vime/datasets.py
@@ -9,14 +9,14 @@ from torch.utils.data import Dataset
 class LabeledDataset(Dataset):
     def __init__(self, X: np.ndarray, y: np.ndarray) -> None:
         assert X.shape[0] == y.shape[0]
-        assert X.ndim == y.ndim == 2
-        assert y.shape[1] == 1
+        assert X.ndim == 2
+        assert y.ndim == 1
         self.X = X
         self.y = y
 
     def __getitem__(self, index: int) -> Tuple[Tensor, Tensor]:
-        x = torch.FloatTensor(self.X[index])
-        y = torch.FloatTensor(self.y[index])
+        x = torch.tensor(self.X[index], dtype=torch.float32)
+        y = torch.tensor(self.y[index], dtype=torch.float32)
         return x, y
 
     def __len__(self) -> int:
@@ -29,7 +29,7 @@ class UnlabeledDataset(Dataset):
         self.X = X
 
     def __getitem__(self, index: int) -> Tensor:
-        x = torch.FloatTensor(self.X[index])
+        x = torch.tensor(self.X[index], dtype=torch.float32)
         return x
 
     def __len__(self) -> int:
